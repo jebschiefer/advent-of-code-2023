2060d5d0e62289849fc965aa06eb7e0b7bac38ee
Jeb Schiefer <jeb.schiefer@modea.com>
2023-12-01 16:48:59 -0500

    Day 1 Part 2

--- day01/day01.go
+++ day01/day01.go
@@ -28,19 +28,84 @@ func GetCalibrationNumber(line string) int {
 	return 0
 }
 
-func SumCalibrationValues(lines string) int {
+func GetCalibrationValues(lines string, convertWords bool) []int {
+	values := []int{}
 	scanner := bufio.NewScanner(strings.NewReader(lines))
 
-	sum := 0
-
 	for scanner.Scan() {
-		calibrationValue := GetCalibrationNumber(scanner.Text())
-		sum += calibrationValue
+		original := scanner.Text()
+		line := scanner.Text()
+
+		if convertWords && original != "" {
+			line = ConvertWordsToInts(line)
+		}
+
+		calibrationValue := GetCalibrationNumber(line)
+		values = append(values, calibrationValue)
 	}
 
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
 
+	return values
+}
+
+func SumCalibrationValues(lines string, convertWords bool) int {
+	values := GetCalibrationValues(lines, convertWords)
+
+	sum := 0
+
+	for _, value := range values {
+		sum += value
+	}
+
 	return sum
+}
+
+func ConvertWordsToInts(line string) string {
+	converted := ""
+
+	for i, character := range line {
+		isInt := character >= '0' && character <= '9'
+		convertedNumber := getNumberSubstring(line, i)
+
+		if isInt {
+			converted += string(character)
+		} else if convertedNumber != "" {
+			converted += convertedNumber
+		}
+	}
+
+	return converted
+}
+
+func getNumberSubstring(line string, offset int) string {
+	number := ""
+
+	wordMapping := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
+
+	for key, value := range wordMapping {
+		end := offset + len(key)
+
+		if end <= len(line) {
+			substring := line[offset:end]
+
+			if substring == key {
+				number = value
+			}
+		}
+	}
+
+	return number
 }
