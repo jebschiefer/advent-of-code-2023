f1b8497872c81836189c86fa683425e7d6084cee
Jeb Schiefer <jeb.schiefer@modea.com>
2023-12-14 16:33:51 -0500

    Day 14 Part 2

--- day14/day14.go
+++ day14/day14.go
@@ -4,9 +4,12 @@ const ROUND = "O"
 const SQUARE = "#"
 const EMPTY = "."
 
-func countLoad(grid [][]string) int {
-	grid = tilt(grid)
+const CLOCKWISE = "clockwise"
+const COUNTER_CLOCKWISE = "counter clockwise"
 
+type Grid [][]string
+
+func countLoad(grid Grid) int {
 	load := 0
 	rowLoadFactor := len(grid)
 
@@ -26,7 +29,7 @@ func countLoad(grid [][]string) int {
 	return load
 }
 
-func tilt(grid [][]string) [][]string {
+func tilt(grid Grid) Grid {
 	for y, row := range grid {
 		if y > 0 {
 			for x, cell := range row {
@@ -47,4 +50,98 @@ func tilt(grid [][]string) [][]string {
 	}
 
 	return grid
+}
+
+func rotate(grid Grid, direction string) Grid {
+	rotated := Grid{}
+	yLength := len(grid)
+
+	if direction == CLOCKWISE {
+		for x := range grid[0] {
+			column := []string{}
+
+			for y := 0; y < yLength; y++ {
+				column = append(column, grid[y][x])
+			}
+
+			rotated = append(rotated, column)
+		}
+	}
+
+	if direction == COUNTER_CLOCKWISE {
+		start := len(grid) - 1
+
+		for x := start; x >= 0; x-- {
+			column := []string{}
+
+			for y := 0; y < yLength; y++ {
+				column = append(column, grid[y][x])
+			}
+
+			rotated = append(rotated, column)
+		}
+	}
+
+	return rotated
+}
+
+func cycle(grid Grid, times int) Grid {
+	for i := 0; i < times; i++ {
+		for j := 0; j < 4; j++ {
+			if j > 0 {
+				for k := 0; k < j; k++ {
+					grid = rotate(grid, CLOCKWISE)
+				}
+			}
+
+			grid = tilt(grid)
+
+			if j > 0 {
+				for k := 0; k < j; k++ {
+					grid = rotate(grid, COUNTER_CLOCKWISE)
+				}
+			}
+		}
+	}
+
+	return grid
+}
+
+func stringify(grid Grid) string {
+	content := ""
+
+	for _, row := range grid {
+		for _, cell := range row {
+			content += cell
+		}
+	}
+
+	return content
+}
+
+func findMinCycles(grid Grid, total int) int {
+	seen := map[string]int{}
+
+	cycleCount := 0
+	cycleLength := 0
+	cycleStart := 0
+	cycleFound := false
+
+	for !cycleFound {
+		key := stringify(grid)
+
+		if seenAtCount, ok := seen[key]; ok {
+			cycleStart = seenAtCount
+			cycleLength = cycleCount - seenAtCount
+			cycleFound = true
+		} else {
+			seen[key] = cycleCount
+			cycleCount++
+			grid = cycle(grid, 1)
+		}
+	}
+
+	remainingCycles := (total - cycleCount) % cycleLength
+
+	return cycleStart + remainingCycles
 }
--- day14/day14_test.go
+++ day14/day14_test.go
@@ -7,18 +7,46 @@ import (
 
 func TestExamplePart1(t *testing.T) {
 	input := utilities.ReadFileToGrid("./samples/example.txt")
+	grid := tilt(input)
 
-	got := countLoad(input)
+	got := countLoad(grid)
 	want := 136
+
+	utilities.CompareInts(t, got, want)
+}
+
+func TestExamplePart2(t *testing.T) {
+	input := utilities.ReadFileToGrid("./samples/example.txt")
+
+	minCycles := findMinCycles(input, 1000000000)
+
+	grid := cycle(input, minCycles)
+
+	got := countLoad(grid)
+	want := 64
 
 	utilities.CompareInts(t, got, want)
 }
 
 func TestPart1(t *testing.T) {
 	input := utilities.ReadFileToGrid("./samples/input.txt")
+	grid := tilt(input)
 
-	got := countLoad(input)
-	want := 136
+	got := countLoad(grid)
+	want := 109661
+
+	utilities.CompareInts(t, got, want)
+}
+
+func TestPart2(t *testing.T) {
+	input := utilities.ReadFileToGrid("./samples/input.txt")
+
+	minCycles := findMinCycles(input, 1000000000)
+
+	grid := cycle(input, minCycles)
+
+	got := countLoad(grid)
+	want := 90176
 
 	utilities.CompareInts(t, got, want)
 }
